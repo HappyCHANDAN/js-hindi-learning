00a3f40b0ab87879962460aafa05d106b31e75db
CHANDAN MAURYA <chandanbhu365@gmail.com>
2026-03-01 05:29:16 +0000

    conversionDataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33vfd" => NaN
+//true => 1; false =0
+console.log(valueInNumber)
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+//1 => true ;  0 =>false
+//"" => false
+//"buddha" => true
+console.log(booleanLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
